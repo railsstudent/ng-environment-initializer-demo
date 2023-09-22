c1d5a65d2491c581ea3b8c86b3df8f8cc8e15f01
Connie Leung <cleung334@gmail.com>
2023-09-22 23:10:59 +0800

    feat: lazy loaded providerCore

--- src/app/users/lazy-loaded/lazy-loaded.component.ts
+++ src/app/users/lazy-loaded/lazy-loaded.component.ts
@@ -1,10 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Injector, inject } from '@angular/core';
 import { providerCore } from '../../core';
 
 @Component({
   selector: 'app-lazy-loaded',
   standalone: true,
   template: '<p>lazy-loaded works!</p>',
-  providers: [providerCore()]
 })
-export class LazyLoadedComponent {}
+export class LazyLoadedComponent {
+  parentInjector = inject(Injector);
+
+  lazyLoadedInjector = Injector.create({
+    providers: [providerCore()],
+    parent: this.parentInjector
+  });
+}
